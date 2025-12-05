1c5cb8693a0724a1880bae77d66a400e1d53fbd2
divya22-dev <divya22.kandregula@gmail.com>
2025-12-05 23:47:41 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -5,7 +5,7 @@ terraform {
   backend "s3" {
     bucket         = "reya-tfstate-bucket-123"
     key            = "3tier/prod/terraform.tfstate"
-    region         = "ap-south-1"
+    region         = "ap-south-2"
     dynamodb_table = "dynamodb-terraform-state-lock"
     encrypt        = true
   }
